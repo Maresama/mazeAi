1b26ffae3f3c6c1dc25037785b1b9aa5cda3da45
1F10250017 <s1F102500179@iniad.org>
2025-06-23 17:15:51 +0900

    直方体の表し方とは

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,34 @@
 'use client';
-
+import { useState } from 'react';
 import styles from './page.module.css';
 
 export default function Home() {
-  return <div className={styles.container} />;
+  const [board, setBoard] = useState([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+  const clickHandler = (x: number, y: number) => {};
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.buttonBoard}>
+        <div className={styles.button}>
+          <p>start</p>
+        </div>
+      </div>
+      <div className={styles.board}>
+        {board.map((row, y) =>
+          row.map((cell, x) => <div key={`${x}-${y}`} className={styles.cell} />),
+        )}
+      </div>
+    </div>
+  );
 }
