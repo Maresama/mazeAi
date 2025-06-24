6329ca0afb7a1b476ada6110965d3a3ea207c03d
1F10250017 <s1F102500179@iniad.org>
2025-06-24 16:01:40 +0900

    エセ迷路作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,31 +2,95 @@
 import { useState } from 'react';
 import styles from './page.module.css';
 
+//4方向
+const directions = [
+  [-1, 0],
+  [0, -1],
+  [0, 1],
+  [1, 0],
+];
+
+//１のマスの周り一つを０から１に変更
+const makeMaze = (board: number[][]) => {
+  //コピー
+  const newBoard = board.map((row) => [...row]);
+
+  for (let y = 0; y < newBoard.length; y++) {
+    for (let x = 0; x < newBoard[0].length; x++) {
+      if (newBoard[y][x] === 1) {
+        //０を入れる箱
+        const candidates: [number, number][] = [];
+
+        for (const [dx, dy] of directions) {
+          const cx = x + dx;
+          const cy = y + dy;
+          if (
+            cy >= 0 &&
+            cy < newBoard.length &&
+            cx >= 0 &&
+            cx < newBoard[0].length &&
+            newBoard[cy][cx] === 0
+          ) {
+            candidates.push([cx, cy]);
+          }
+        }
+        if (candidates.length > 0) {
+          const [cx, cy] = candidates[Math.floor(Math.random() * candidates.length)];
+          newBoard[cy][cx] = 1;
+        }
+      }
+    }
+  }
+  return newBoard;
+};
+
 export default function Home() {
   const [board, setBoard] = useState([
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   ]);
-  const clickHandler = (x: number, y: number) => {};
+  console.log(board);
+  const handleClick = () => {
+    const newBoard = makeMaze(board);
+    setBoard(newBoard);
+  };
 
   return (
     <div className={styles.container}>
       <div className={styles.buttonBoard}>
-        <div className={styles.button}>
+        <button onClick={handleClick} className={styles.button}>
           <p>start</p>
-        </div>
+        </button>
       </div>
       <div className={styles.board}>
         {board.map((row, y) =>
-          row.map((cell, x) => <div key={`${x}-${y}`} className={styles.cell} />),
+          row.map((cell, x) => (
+            <div
+              key={`${x}-${y}`}
+              className={styles.cell}
+              style={{ backgroundColor: cell === 1 ? '#111111' : '#ffffff' }}
+            />
+          )),
         )}
       </div>
     </div>
